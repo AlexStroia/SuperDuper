060fb6e76876d970e2025912ad5cc2106e4b7472
Alex S <alex.s@smarthuts.com>
2021-03-27 14:43:36 +0200

    Clear error

--- nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,16 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
-import com.udacity.jwdnd.course1.cloudstorage.pages.Home;
 import com.udacity.jwdnd.course1.cloudstorage.pages.Login;
 import com.udacity.jwdnd.course1.cloudstorage.pages.Signup;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
