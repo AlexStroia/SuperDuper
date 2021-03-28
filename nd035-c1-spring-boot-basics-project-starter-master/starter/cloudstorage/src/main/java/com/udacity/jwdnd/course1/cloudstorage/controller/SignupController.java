4871fdf5e0cb00375ff010ae3f20b4c32b061856
Alex S <alex.s@smarthuts.com>
2021-03-28 20:38:09 +0300

    Coderview issues

--- nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -41,7 +41,7 @@ public class SignupController {
         }
 
         if (signupError == null) {
-            model.addAttribute("signupSuccess", true);
+            redirectAttributes.addFlashAttribute("signupSuccess", true);
             return "redirect:/login";
         } else {
             model.addAttribute("signupError", signupError);
